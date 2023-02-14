032cb38d1fa6ad8d83c9a0623434df0ccb479cc7
SamuelMonin <samuel.monin2003@gmail.com>
2023-02-14 19:56:07 +0100

    Change your current score.

--- src/Quiz/score.js
+++ src/Quiz/score.js
@@ -4,9 +4,10 @@ import Stack from '@mui/material/Stack';
 
 export function Score () {
     const score = useSelector((state) => state.counter.score)
+    const index = useSelector((state) => state.counter.index)
     return (
         <Stack>
-            <p>Your current score : {score}/10</p>
+            <p>Your current score : {score}/{index + 1}</p>
         </Stack>
     )
 }
